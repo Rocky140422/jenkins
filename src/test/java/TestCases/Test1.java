2cd8595fc6c47e025059e4697c3ea388332a6b03
Rocky140422 <vishwas8788@gmail.com>
2022-05-08 18:12:52 +0530

    This is jenkin practise

---
+++ src/test/java/TestCases/Test1.java
@@ -0,0 +1,21 @@
+package TestCases;
+
+public class Test1 {
+
+	@org.testng.annotations.Test
+	public void Test1() {
+		System.out.println("This is Test 1");
+	}
+	
+
+	@org.testng.annotations.Test
+	public void Test2() {
+		System.out.println("This is Test 2");
+	}
+	
+
+	@org.testng.annotations.Test
+	public void Test3() {
+		System.out.println("This is Test 3");
+	}
+}
